690eaec5d39b63e7d9cf8affaef244ea34ea38a2
GMoraesGarcia <g.moraes.garcia@gmail.com>
2021-05-28 20:52:31 -0300

    layout comentarios

--- comentarios/src/main/java/br/senac/tads/dsw/comentarios/comentario/Comentario.java
+++ comentarios/src/main/java/br/senac/tads/dsw/comentarios/comentario/Comentario.java
@@ -5,31 +5,56 @@
  */
 package br.senac.tads.dsw.comentarios.comentario;
 
+import br.senac.tads.dsw.comentarios.produto.Produto;
+import java.time.LocalDateTime;
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToOne;
+import javax.persistence.Transient;
+
 /**
  *
  * @author Bigstudios
  */
 
+@Entity
 public class Comentario {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     
+    @Column
     private String nome;
     
+    @Column(unique = true)
     private String email;
     
+    @Column
     private String comentario;
+    
+    @Column
+    private LocalDateTime dataHorario;
+   
+   @OneToOne(cascade = CascadeType.ALL)
+   private Produto produto;
 
     public Comentario() {
     }
 
-    public Comentario(String nome, String email, String comentario) {
+    public Comentario(String nome, String email, String comentario, LocalDateTime dataHorario, Produto produto) {
         this.nome = nome;
         this.email = email;
         this.comentario = comentario;
+        this.dataHorario = dataHorario;
+        this.produto = produto;
     }
 
-    
-    
+ 
     public Long getId() {
         return id;
     }
@@ -60,6 +85,22 @@ public class Comentario {
 
     public void setComentario(String comentario) {
         this.comentario = comentario;
+    }
+
+    public LocalDateTime getDataHorario() {
+        return dataHorario;
+    }
+
+    public void setDataHorario(LocalDateTime dataHorario) {
+        this.dataHorario = dataHorario;
+    }
+
+    public Produto getProduto() {
+        return produto;
+    }
+
+   public void setProduto(Produto produto) {
+        this.produto = produto;
     }
     
     
